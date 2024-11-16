579ccbea36e9587f1b40a42059cb898aa15c1b8a
musiur <musiur.opu@gmail.com>
2024-11-17 01:50:37 +0600

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="text-9xl font-bold text-primary bg-secondary">Home Page</div>
+    <div className="text-9xl font-bold text-primary bg-secondary">New Home Page</div>
   );
 };
 
